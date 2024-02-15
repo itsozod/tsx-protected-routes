eadcaf390ac2bb1afcd0e9e4d420e4663420c278
Ozod <o.bukhorizoda@mail.ru>
2024-02-15 10:22:16 +0500

    add bem architechture

--- src/pages/todos/Todos.tsx
+++ src/pages/todos/Todos.tsx
@@ -1,12 +1,19 @@
+import { Button } from "antd";
 import { useGetTodosQuery } from "../../store/api/api";
-import styles from "./Todos.module.css";
+import styles from "./Todos.module.scss";
 
 export const Todos = () => {
   const { data } = useGetTodosQuery();
   return (
     <div className={styles.todos_container}>
       {data?.map((todo) => {
-        return <div key={todo.id}>{todo.title}</div>;
+        return (
+          <div className={styles.todo_card} key={todo.id}>
+            <p>{todo.title}</p>
+            <Button className={styles.todo_card__edit_btn}>Edit</Button>
+            <Button className={styles.todo_card__delete_btn}>Delete</Button>
+          </div>
+        );
       })}
     </div>
   );
